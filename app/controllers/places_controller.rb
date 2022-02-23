d03e48c9a19e9f795c1e3f87ce852d8a53d3e7e8
9x-nik <nikhil.nimmagadda@kellogg.northwestern.edu>
2022-02-23 21:11:56 +0000

    commit 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,25 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @company = Places.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
+    def show
+        @place = Place.find(params["id"])
+    end
+    
+end
